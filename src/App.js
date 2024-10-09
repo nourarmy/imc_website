9a5f0ef3ba7910ed1348191b0a5ff4f57a65ab33
Nour ben chaaben <144450617+nourarmy@users.noreply.github.com>
2024-10-09 21:31:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import Services from './components/Services';
 import Team from './components/Team';
-import Upcomingevents from './components/Upcomingevents';
+import UpcomingEvents from './components/UpcomingEvents';
 import Footer from './components/Footer';
 import CountdownTimer from './components/CountdownTimer';
-import Preloader from './components/Preloader';
+// import Preloader from './components/Preloader'; // Comment out this line
 
 // Import images
 import logo1 from './components/img/logo1.png';
@@ -19,43 +19,57 @@ import logo5 from './components/img/logo5.png';
 function App() {
   const eventDate = '2024-10-27T00:00:00Z'; // Your next event date
   const images = [logo1, logo2, logo3, logo4, logo5]; // Use imported images here
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true); // Comment out loading state
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-  }, []);
-  
+  // Preloader effect
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 5000); // Loading duration (5 seconds)
+
+  //   return () => clearTimeout(timer); // Cleanup timeout
+  // }, []);
+
   return (
     <div className="App">
-      {isLoading ? (
-        <Preloader images={images} />
-      ) : (
-        <>
-          <CountdownTimer eventDate={eventDate} />
-          <div id="home">
-            <Home />
-          </div>
-          <div id="about-us">
-            <AboutUs />
-          </div>
-          <div id="services">
-            <Services />
-          </div>
-          <div id="upcoming-events">
-            <Upcomingevents />
-          </div>
-          <div id="team">
-            <Team />
-          </div>
-          <div id="join-us">
-            <Footer />
-          </div>
-        </>
-      )}
+      {/* Temporarily render main content directly */}
+      <>
+        {/* Countdown Timer for the next event */}
+        <CountdownTimer eventDate={eventDate} />
+
+        {/* Home Section */}
+        <div id="home">
+          <Home />
+        </div>
+
+        {/* About Us Section */}
+        <div id="about-us">
+          <AboutUs />
+        </div>
+
+        {/* Services Section */}
+        <div id="services">
+          <Services />
+        </div>
+
+        {/* Upcoming Events Section */}
+        <div id="upcoming-events">
+          <UpcomingEvents />
+        </div>
+
+        {/* Team Section */}
+        <div id="team">
+          <Team />
+        </div>
+
+        {/* Footer Section */}
+        <div id="join-us">
+          <Footer />
+        </div>
+      </>
     </div>
   );
 }
 
 export default App;
+y
